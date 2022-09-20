36ac1feb677e3e2aadf58613cd5c300a42ffbfca
WrittenInnaCode <writteninnacode@gmail.com>
2022-09-20 15:29:41 -0700

    added remove function

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -22,4 +22,21 @@ const readAndAppend = (content, file) => {
     });
   };
   
+
+  const readAndRemove = (id, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        for (let i = 0; i < parsedData.length; i++) {
+          if (id === parsedData[i].id){
+            parsedData.splice(i, 1);
+            writeToFile(file, parsedData);
+          }
+        }
+      }
+    });
+  };
+
   module.exports = { readFromFile, writeToFile, readAndAppend };
